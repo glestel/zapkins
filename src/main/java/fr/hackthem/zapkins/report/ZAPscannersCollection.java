f715e8c324ad8ab4b853225bfeae1d5dee5b7122
GDRB6297 <GDRB6297@EB-OR7117770.bagnolet.francetelecom.fr>
2016-03-07 14:14:32 +0100

    -Update the scanners list -Remove unused class ZAPauthenticationScriptsCollection

--- src/main/java/fr/hackthem/zapkins/report/ZAPscannersCollection.java
+++ src/main/java/fr/hackthem/zapkins/report/ZAPscannersCollection.java
@@ -57,33 +57,63 @@ public class ZAPscannersCollection implements Serializable {
 		// Add SCANNERS to the map
 		mapScannersTypes.put("ALL SCANNERS", "ALL");
 		
+		
+		
 		mapScannersTypes.put("Cross Site Scripting (Reflected)", "40012");
 		mapScannersTypes.put("Cross Site Scripting (Persistent)", "40014");
 		mapScannersTypes.put("Cross Site Scripting (Persistent) - Prime", "40016");
 		mapScannersTypes.put("Cross Site Scripting (Persistent) - Spider", "40017");
+		
 		mapScannersTypes.put("SQL Injection", "40018");
+		mapScannersTypes.put("Session Fixation", "40013");
+		mapScannersTypes.put("LDAP Injection", "40015");
+		mapScannersTypes.put("SQL Injection - MySQL", "40019");
+		mapScannersTypes.put("SQL Injection - Hypersonic SQL", "40020");
+		mapScannersTypes.put("SQL Injection - Oracle", "40021");
+		mapScannersTypes.put("SQL Injection - PostgreSQL", "40022");
+		mapScannersTypes.put("Advanced SQL Injection", "90018");
+		mapScannersTypes.put("SQL Injection - SQLite", "40024");
+		 
 		
-//		mapScannersTypes.put("Anti CSRF tokens scanner", "20012");
-//		mapScannersTypes.put("HTTP Parameter Pollution scanner", "20014");
-//		mapScannersTypes.put("Session Fixation", "40013");
-//		mapScannersTypes.put("LDAP Injection", "40015");
-//		mapScannersTypes.put("SQL Injection - MySQL", "40019");
-//		mapScannersTypes.put("SQL Injection - Hypersonic SQL", "40020");
-//		mapScannersTypes.put("SQL Injection - Oracle", "40021");
-//		mapScannersTypes.put("SQL Injection - PostgreSQL", "40022");
-//		mapScannersTypes.put("Possible Username Enumeration", "40023");
-//		mapScannersTypes.put("XPath Injection Plugin", "90021");
-//		mapScannersTypes.put("XML External Entity Attack", "90023");
-//		
+		mapScannersTypes.put("Anti CSRF tokens scanner", "20012");
+		mapScannersTypes.put("HTTP Parameter Pollution scanner", "20014");
+		
+		mapScannersTypes.put("Possible Username Enumeration", "40023");
+		mapScannersTypes.put("XPath Injection Plugin", "90021");
+		mapScannersTypes.put("XML External Entity Attack", "90023");
+		
+		mapScannersTypes.put("Generic Padding Oracle", "90024");
+		mapScannersTypes.put("Cookie Slack Detector","90027");
+		mapScannersTypes.put("Expression Language Injection","90025");
+		mapScannersTypes.put("Backup File Disclosure","10095");
+		mapScannersTypes.put("Integer Overflow Error","30003");
+		mapScannersTypes.put("Insecure HTTP Method","90028");
+		
+		mapScannersTypes.put("HTTP Parameter Pollution scanner","20014");
+		
+		mapScannersTypes.put("Remote Code Execution - CVE-2012-1823","20018");
+		mapScannersTypes.put("Source Code Disclosure - CVE-2012-1823","20017");
+		mapScannersTypes.put("Cross-Domain Misconfiguration","20016");
+		mapScannersTypes.put("Heartbleed OpenSSL Vulnerability","2015");
+		mapScannersTypes.put("Anti CSRF Tokens Scanner","20012");
+		
+		mapScannersTypes.put("Remote Code Execution - Shell Shock","10048");
+		mapScannersTypes.put("Source Code Disclosure - /WEB-INF folder", "10045");
+		mapScannersTypes.put("Source Code Disclosure - SVN", "42");
+		mapScannersTypes.put("Source Code Disclosure - Git","41"); 
+		mapScannersTypes.put("Source Code Disclosure - File Inclusion","43");
 		
 		
+		mapScannersTypes.put("Script Active Scan Rules", "50000");
+		mapScannersTypes.put("Buffer Overflow", "30001");
+		mapScannersTypes.put("Format String Error", "30002");
 		mapScannersTypes.put("Server Side Code Injection", "90019");
-		mapScannersTypes.put(" Remote OS Command Injection Plugin", "90020");
+		mapScannersTypes.put("Remote OS Command Injection Plugin", "90020");
 		mapScannersTypes.put("Directory browsing", "0");
 		mapScannersTypes.put("Path Traversal", "6");
 		mapScannersTypes.put("Remote File Inclusion", "7");
 		mapScannersTypes.put("Secure page browser cache", "10001");
-		mapScannersTypes.put("External redirect", "30000");
+		mapScannersTypes.put("External redirect", "20019");
 		mapScannersTypes.put("CRLF injection", "40003");
 		mapScannersTypes.put("Parameter tampering", "40008");
 		mapScannersTypes.put("Server side include", "40009");
